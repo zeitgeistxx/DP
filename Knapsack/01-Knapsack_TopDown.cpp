8d4694a4df336b31dc7415681e96253b6fe3579a
ABHIRUP <abhirupsaha02@gmail.com>
2023-06-20 04:46:55 +0530

    TopDown Knapsack

--- Knapsack/01-Knapsack_TopDown.cpp
+++ Knapsack/01-Knapsack_TopDown.cpp
@@ -1,15 +1,41 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-
-
-struct Bag{
+struct Bag
+{
     int profit, weight;
 };
 
+int t[101][1001]; // t[n+1][W+1]
 
-int main(){
+int Knapsack(Bag b[], int W, int n)
+{
+    // Initialization
+    for (int i = 0; i < n + 1; i++)
+        t[i][0] = 0;
+    for (int i = 0; i < W + 1; i++)
+        t[0][i] = 0;
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < W + 1; j++)
+        {
+            if (b[i - 1].weight <= j)
+            {
+                t[i][j] = max(b[i - 1].profit + t[i - 1][j - b[i - 1].weight], t[i - 1][j]);
+            }
+            else
+            {
+                t[i][j] = t[i - 1][j];
+            }
+        }
+    }
+    return t[n][W];
+}
+
+int main()
+{
     int n, W;
     cout << "Enter no. of Bags and Kanpsack capacity -> ";
     cin >> n >> W;
@@ -17,7 +43,8 @@ int main(){
     cout << "\n\n";
 
     Bag b[n];
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cout << "Bag no. " << i + 1 << ":" << endl;
         cout << "Profit -> ";
         cin >> b[i].profit;
